8c10f1bc9a77050a980ba7269be17c582a12ab00
Justas Balcas <juztas@gmail.com>
2022-03-03 08:53:02 -0800

    Condor WN container

---
+++ condor-wn/upload.sh
@@ -0,0 +1,5 @@
+SWARM=<REPLACE_ME>
+docker login $SWARM
+dockerimageid=`docker images | grep condor-wn | grep latest | awk '{print $3}'`
+docker tag $dockerimageid $SWARM/condor-wn:latest
+docker push $SWARM/condor-wn
