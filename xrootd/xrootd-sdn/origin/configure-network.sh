86326b31b60dc9a3a492b1eb39736b564768983a
Justas Balcas <juztas@gmail.com>
2022-09-12 16:06:07 -0700

    Update xrootd config

--- xrootd/xrootd-sdn/origin/configure-network.sh
+++ xrootd/xrootd-sdn/origin/configure-network.sh
@@ -2,7 +2,6 @@
 # Disable autoconf.
 sysctl -w net.ipv6.conf.default.autoconf=0
 
-# 2605:d9c0:2:fff0::2/64
 export ID=0
 export END=2
 # echo /etc/iproute2/rt_tables new macvlan….
@@ -11,13 +10,11 @@ ip link add link macvlan$ID link enp143s0.43 type macvlan mode bridge
 ip link set macvlan$ID up
 ip -6 addr add 2605:d9c0:2:fff$ID::$END/64 dev macvlan$ID
 ip -6 r add 2605:d9c0:2:fff$ID::$END dev macvlan$ID table macvlan$ID
-while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 ip -6 route add default via 2605:d9c0:2:fff$ID::1 dev macvlan$ID table macvlan$ID
 ip -6 rule add from 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
 ip -6 rule add to 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
+while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 
-
-# 2605:d9c0:2:fff1::2/64
 export ID=1
 export END=2
 # echo /etc/iproute2/rt_tables new macvlan….
@@ -26,13 +23,12 @@ ip link add link macvlan$ID link enp143s0.43 type macvlan mode bridge
 ip link set macvlan$ID up
 ip -6 addr add 2605:d9c0:2:fff$ID::$END/64 dev macvlan$ID
 ip -6 r add 2605:d9c0:2:fff$ID::$END dev macvlan$ID table macvlan$ID
-while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 ip -6 route add default via 2605:d9c0:2:fff$ID::1 dev macvlan$ID table macvlan$ID
 ip -6 rule add from 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
 ip -6 rule add to 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
+while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 
 
-# 2605:d9c0:2:fff2::2/64
 export ID=2
 export END=2
 # echo /etc/iproute2/rt_tables new macvlan….
@@ -41,14 +37,12 @@ ip link add link macvlan$ID link enp143s0.43 type macvlan mode bridge
 ip link set macvlan$ID up
 ip -6 addr add 2605:d9c0:2:fff$ID::$END/64 dev macvlan$ID
 ip -6 r add 2605:d9c0:2:fff$ID::$END dev macvlan$ID table macvlan$ID
-while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 ip -6 route add default via 2605:d9c0:2:fff$ID::1 dev macvlan$ID table macvlan$ID
 ip -6 rule add from 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
 ip -6 rule add to 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
+while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 
 
-
-# 2605:d9c0:2:fff3::2/64
 export ID=3
 export END=2
 # echo /etc/iproute2/rt_tables new macvlan….
@@ -57,8 +51,7 @@ ip link add link macvlan$ID link enp143s0.43 type macvlan mode bridge
 ip link set macvlan$ID up
 ip -6 addr add 2605:d9c0:2:fff$ID::$END/64 dev macvlan$ID
 ip -6 r add 2605:d9c0:2:fff$ID::$END dev macvlan$ID table macvlan$ID
-while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
 ip -6 route add default via 2605:d9c0:2:fff$ID::1 dev macvlan$ID table macvlan$ID
 ip -6 rule add from 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
 ip -6 rule add to 2605:d9c0:2:fff$ID::$END/128 table macvlan$ID
-
+while true; do ping6 -c2 2605:d9c0:2:fff$ID::1 -I macvlan$ID > /dev/null && break; done
