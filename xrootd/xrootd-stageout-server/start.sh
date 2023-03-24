fbbfa473e5287a47104eebc83f86c9b92ab070e0
Justas Balcas <juztas@gmail.com>
2023-03-23 17:08:54 -0700

    Update XRootD for transfer nodes to latest release

--- xrootd/xrootd-stageout-server/start.sh
+++ xrootd/xrootd-stageout-server/start.sh
@@ -1,16 +1,5 @@
 source ../../environment
 
 HOST=`hostname -s`
-FILE=config/$HOST/etc/t2_maitenance.conf
-if [ ! -f "$FILE" ]; then
-   echo "========================================================="
-   echo " CONFIGURATION FAILURE!!!"
-   echo "========================================================="
-   echo "Prepare monitoring configuration file:"
-   echo "1. cp config/default/etc/t2_maitenance.conf $FILE"
-   echo "2. edit $FILE and update IPs and or other parameters as needed"
-   echo "========================================================="
-   exit 1
-fi
 
 docker-compose up --detach
