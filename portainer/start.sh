76797e5f28f2fa9e4ee7489ee2d1b870bf4f0c39
Justas Balcas <juztas@gmail.com>
2023-12-15 06:09:08 -0800

    Remove unused images

--- portainer/start.sh
+++
@@ -1,3 +0,0 @@
-# Need to check that volume is present
-#docker volume create portainer_data
-docker run -d --net=host --name portainer     --restart=always     -v /var/run/docker.sock:/var/run/docker.sock     -v portainer_data:/data     cr.portainer.io/portainer/portainer-ce:2.11.1
--- xrootd/fts/upload-sense.sh
+++
@@ -1,5 +0,0 @@
-source ../../environment
-docker login
-dockerimageid=`docker images | grep sense-fts3 | grep latest | awk '{print $3}'`
-docker tag $dockerimageid sdnsense/sense-fts3:latest
-docker push sdnsense/sense-fts3
