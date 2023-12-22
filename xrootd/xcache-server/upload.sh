b6485f022cbd1643cf45f0344e6135fd68f64484
Justas Balcas <juztas@gmail.com>
2023-12-22 08:24:12 -0800

    Update xrootd xcache to latest release and kube

--- xrootd/xcache-server/upload.sh
+++ xrootd/xcache-server/upload.sh
@@ -1,5 +1,5 @@
 source ../../environment
-docker login $SWARM
+docker login -u $USERNAME
 dockerimageid=`docker images | grep xcache-server | grep latest | awk '{print $3}'`
-docker tag $dockerimageid $SWARM/xcache-server:latest
-docker push $SWARM/xcache-server
+docker tag $dockerimageid $USERNAME/xcache-server:latest
+docker push $USERNAME/xcache-server
