9c3222ba3f493e77159427f56391795cbf8642a1
Justas Balcas <juztas@gmail.com>
2024-01-22 07:41:45 -0600

    Add git build for xcache, nomachine, xrootd stageout and sdn

--- xrootd/xrootd-stageout-server/build.sh
+++ xrootd/xrootd-stageout-server/build.sh
@@ -2,4 +2,4 @@ for did in `docker images | grep xrootd-stageout-server | awk '{print $3}' | uni
 do
   docker image rm $did --force;
 done
-docker build --no-cache -t xrootd-stageout-server .
+docker build --no-cache -t xrootd-stageout-server --platform=linux/amd64 .
