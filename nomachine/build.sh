97c46e1852d913579737fac59c8af7ef7cf9984c
Justas Balcas <juztas@gmail.com>
2024-01-22 14:09:02 -0600

    build update

--- nomachine/build.sh
+++ nomachine/build.sh
@@ -2,4 +2,4 @@ for did in `docker images | grep hepgui | awk '{print $3}' | uniq`;
 do
   docker image rm $did --force;
 done
-docker build --no-cache -t hepgui .
+docker build --no-cache -t nomachine --platform=linux/amd64 .
