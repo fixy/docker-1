92d841313114eba456f8c7d39e860ac977fb0f5e
Raimondas Sirvinskas <raimis.sirvis@gmail.com>
2024-03-10 15:29:56 +0200

    Add FDT Dockerfile for FDT and FDT Agent

--- fdt-agent/upload.sh
+++
@@ -1,5 +0,0 @@
-source ../environment
-docker login
-dockerimageid=`docker images | grep hepgui | grep latest | awk '{print $3}'`
-docker tag $dockerimageid cmscaltech/hepgui:latest
-docker push cmscaltech/hepgui
